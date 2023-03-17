1928dfe22d4bb62aec65b55ae031ab1fcb11abf6
Vitor <vitor.daniel@outlook.pt>
2023-03-17 11:36:37 +0000

    forgot to commit ex 1, started ex 2

---
+++ ArraySums/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArraySums
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("insert number for columns and rows: ");
+            int dim = Convert.ToInt32(Console.ReadLine());
+            int[ , ] matrix = new int [dim, dim];
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                {
+                    Console.WriteLine("nr pos (" + i + ", " + j + ")");
+                    int input = Console.Read();
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ChangeString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main()
+        {
+            string a = Console.ReadLine();
+            char b = Convert.ToChar(Console.ReadLine());
+            for(int i = 0; i < a.Length; i++)
+            {
+                if(a[i] != b)
+                {
+                    Console.Write(a[i]);
+                } 
+            }
+        }
+    }
+}
